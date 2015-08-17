38a36fe2c521d6b2aa8c4456f1b2f59ae55795f2
pongtsu <pongtsu@bitbucket.com>
2015-08-17 11:40:43 +0700

    add another

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', function (req, res) {
   res.send('hello jenkins');
 });
 
+app.get('/hello', function (req, res) {
+  res.send('hello world');
+});
+
 app.listen(process.env.PORT || 5000);
 
 module.exports = app;
